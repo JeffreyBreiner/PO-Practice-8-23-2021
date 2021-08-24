3cd3e9a66f53c3768d515d503bf6171b5b036d62
Jeffrey Breiner <jeffreybreiner99@gmail.com>
2021-08-24 14:29:33 -0400

    All Tests Passed

---
+++ PO-Practice-8-23-2021/PO-Practice-8-23-2021/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace PO_Practice_8_23_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string socks = String.Concat(Console.ReadLine().OrderBy(c => c));
+            int pairs = 0;
+            char current = ' ';
+
+            foreach (char letter in socks)
+            {
+                if (current == letter)
+                {
+                    current = ' ';
+                    pairs++;
+                }
+                else
+                {
+                    current = letter;
+                }
+            }
+
+            Console.WriteLine(pairs);
+        }
+    }
+}
